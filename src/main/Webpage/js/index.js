50e2657926b23d2cb5a6a9e39e28410b4dde6514
jjgrayg <jjgrayg@hotmail.com>
2022-02-23 11:17:40 -0500

    0.1.8 Started writing storyline Somewhat began implementation on dynamic landing page

---
+++ src/main/Webpage/js/index.js
@@ -0,0 +1,54 @@
+import {TextPlugin} from "./gsap/TextPlugin.js";
+
+function destroyBody() {
+    $("#header").off("click");
+
+    let t1 = gsap.timeline();
+    let text1 = $("#header h1");
+    t1.flickerTo(text1, {duration: 5,
+            textAlign: "center"})
+        .flickerTo(text1, {text: "Leave",
+            duration: 0.002,
+            textAlign: "center"});
+
+    let t2 = gsap.timeline();
+    let text2 = $("#textbody");
+    t2.flickerTo(text2, {text: "<p></p>", duration: 0.0002});
+    t2.flickerTo(text2, {text: "<p></p>", duration: 10});
+    t2.flickerTo(text2, {text: "<p>Come back in 5 minutes. Give us some time to fix what <b style='color: rgb(255, 0, 0)'><i>you</i></b> have broken.</p>",
+        duration: 0,
+        textAlign: "center"});
+}
+
+function isVisible(querySelector) {
+    const box = document.querySelector('#header');
+    const rect = box.getBoundingClientRect();
+
+    const isInViewport = rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+    return isInViewport;
+}
+
+function addEventListeners() {
+    $("#header").on("click", destroyBody);
+}
+
+// Start script
+window.onload = function () {
+    gsap.registerPlugin(TextPlugin);
+    gsap.registerEffect({
+        name: "flickerTo",
+        effect: (targets, config) => {
+            return gsap.to(targets, {duration: config.duration,
+                text: config.text,
+                textAlign: config.textAlign,
+                ease: "none"});
+        },
+        defaults: {duration: 2, text: "Welcome", textAlign: "left"}, //defaults get applied to any "config" object passed to the effect
+        extendTimeline: true, //now you can call the effect directly on any GSAP timeline to have the result immediately inserted in the position you define (default is sequenced at the end)
+    });
+    addEventListeners();
+}
--- src/main/java/com/InfiniteDark/Controllers/RoutingController.java
+++ src/main/java/com/InfiniteDark/Controllers/RoutingController.java
@@ -14,32 +14,12 @@ import java.io.File;
 public class RoutingController {
 
 
-    DateTime tempDate = new DateTime().plus(30 * 1000);
-    ClassLoader classLoader = getClass().getClassLoader();
-
     @GetMapping("/")
     public String base() {
         ExtendedDocFactory docFactory = new ExtendedDocFactory();
-        String resource = new File("src/main/HTML/test.html").getPath();
-        String[] temp = {".active", "#title-container"};
+        String resource = new File("src/main/Webpage/index.html").getPath();
         ExtendedDoc t = docFactory.createExtendedDoc(resource);
-        HTMLBehavior newBehavior = new HTMLBehavior("#test-num");
-        newBehavior.setBehavior("Wafflehouse", Comparison.EQUAL, 100);
-        t.addBehavior(temp[0], "<a href=\"https://google.com\">Do not click</a>", tempDate);
-        t.addBehavior(temp[1], "<h1>Taco Tuesday</h1>", tempDate);
-        t.addBehavior(newBehavior);
-        t.executeBehaviors();
         return t.getStringDoc();
-//        ExtendedDoc t;
-//        try {
-//            t = new ExtendedDoc().createFromJson("src/main/ExtendedDocs/testhtml_ExtendedDoc.json");
-//            t.executeBehaviors();
-//            return t.getStringDoc();
-//        }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return "Error";
     }
 
 }
